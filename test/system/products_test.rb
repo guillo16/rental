d379c13d68e8eeb1c9ce6a4c9dd9e2e7d5e9a405
guillermo varela <guillovarela16@gmail.com>
2020-04-06 10:49:18 -0300

    controller generated

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
-    visit products_url
+    visit "/"
 
     assert_selector "h1", text: "Product"
   end

186a86f4460206d674e25de0c1e7b12d77aebfce
guillermo varela <guillovarela16@gmail.com>
2020-04-06 18:31:21 -0300

    new validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#product' do
+    user = User.create(email: 'guillo@hotmail.com', password: 123456 )
+    product1 = Product.create(title: 'John', description: 'some description', user: User.last, category: Category.last)
+    product2 = Product.create(title: 'John', description: 'some description', user: User.last, category: Category.last)
+
+    assert_equal 2, user.products.size
+  end
 end

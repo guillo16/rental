6ac0a2a3a71ec10cad012356452891afb7afbeac
guillermo varela <guillovarela16@gmail.com>
2020-04-06 18:00:04 -0300

    model validation finish

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+ test 'valid category' do
+  category = Category.new(title: 'hola')
+  assert category.valid?
+end
+
+test 'invalid without title' do
+  category = Category.new
+  refute category.valid?, 'category is valid without a title'
+  assert_not_nil category.errors[:title], 'no validation error for title present'
+end
 end

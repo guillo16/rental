d9649b684980def9188fb6a717150d7e752159c0
guillermo varela <guillovarela16@gmail.com>
2020-04-07 19:50:05 -0300

    finish the backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @products = Product.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
+  resources :users, only: [:show]
+
   resources :bookings, only: [:index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

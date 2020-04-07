365d2b027a93c68c175722c659f27c6b8814f5a2
guillermo varela <guillovarela16@gmail.com>
2020-04-07 11:07:49 -0300

    finish migration

---
+++ db/migrate/20200407140656_add_start_to_booking.rb
@@ -0,0 +1,7 @@
+class AddStartToBooking < ActiveRecord::Migration[5.2]
+  def change
+    add_column :bookings, :start, :datetime
+    add_column :bookings, :end, :datetime
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_213811) do
+ActiveRecord::Schema.define(version: 2020_04_07_140656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_04_06_213811) do
     t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start"
+    t.datetime "end"
     t.index ["product_id"], name: "index_bookings_on_product_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end

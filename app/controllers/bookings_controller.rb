26ad1640aedb8ac2961d7cfc264d0f835fe69189
guillermo varela <guillovarela16@gmail.com>
2020-04-07 11:18:43 -0300

    time handle

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -26,6 +26,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:date, :start_time, :end_time)
+    params.require(:booking).permit(:date, :start, :end)
   end
 end

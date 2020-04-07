f92d6dba38612506713c59595a946c3c53595c81
guillermo varela <guillovarela16@gmail.com>
2020-04-07 20:09:07 -0300

    create new something

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,13 @@ puts 'creating Products'
 
 category = Category.all.sample
 user = User.all.sample
-
+photo = ['https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+  'https://images.unsplash.com/photo-1535732820275-9ffd998cac22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1570294646112-27ce4f174e38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2068&q=80',
+  'https://images.unsplash.com/flagged/photo-1553505192-acca7d4509be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1067&q=80'
+].sample
 
 10.times do
-Products.create! title: '', description: '', photo: '', category: , user:
-
+  Product.create! title: Faker::Vehicle.make_and_model, description: Faker::Vehicle.standard_specs , photo: photo, category: category , user: user
+end
+puts 'finish seeds'

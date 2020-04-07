cc62819b86191dee404dc108791df099f9fb9fcb
guillermo varela <guillovarela16@gmail.com>
2020-04-07 20:01:01 -0300

    faker added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'destroy database'
+
+Booking.delete_all
+Product.delete_all
+Category.delete_all
+User.delete_all
+
+puts 'creating user'
+
+user1 = User.create! email: 'user1@mail.com', password: 123456
+user2 = User.create! email: 'user2@mail.com', password: 123456
+user3 = User.create! email: 'user3@mail.com', password: 123456
+
+puts 'creating Categories'
+
+supercars = Category.create! title: 'supercar'
+suv = Category.create! title: 'suv'
+luxury = Category.create! title: 'luxury'
+
+puts 'creating Products'
+
+category = Category.all.sample
+user = User.all.sample
+
+
+10.times do
+Products.create! title: '', description: '', photo: '', category: , user:
+

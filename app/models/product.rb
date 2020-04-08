cfe66ea95ebe2efbf3d6b1c53e4b79e9468b721c
guillermo varela <guillovarela16@gmail.com>
2020-04-08 18:26:09 -0300

    add address to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,7 @@ class Product < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
